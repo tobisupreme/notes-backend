b70e3196990d0540f2b37c80b7baa7ebb78cab20
Tobi Balogun <tobi.balo@gmail.com>
2022-10-09 13:46:25 +0100

    func: add test for checking uniqueness of username at creation

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -148,6 +148,28 @@ describe('when there is initially one user in db', () => {
     const usernames = usersAtEnd.map(user => user.username)
     expect(usernames).toContain(newUser.username)
   })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'admin',
+      name: 'Superuser',
+      password: 'securepassword'
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('username must be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtStart.length).toBe(usersAtEnd.length)
+    expect(usersAtStart).toEqual(usersAtEnd)
+  })
 })
 
 afterAll(() => {
