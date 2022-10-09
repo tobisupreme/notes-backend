f77925ee8455ae31e277c65e9294e723ff374a13
Tobi Balogun <tobi.balo@gmail.com>
2022-10-09 10:42:57 +0100

    func: add functionality for creating new users in the database

---
+++ controllers/users.js
@@ -0,0 +1,21 @@
+const usersRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+usersRouter.post('/', async (req, res) => {
+  const { username, password, name } = req.body
+
+  const passwordHash = await bcrypt.hash(password, 10)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash
+  })
+
+  const savedUser = await user.save()
+
+  res.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
