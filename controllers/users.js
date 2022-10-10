b5887307ed116c0b67bce8d75835c9ed45453cdf
Tobi Balogun <tobi.balo@gmail.com>
2022-10-10 16:03:39 +0100

    func: implement route handler that returns all the users in the database

--- controllers/users.js
+++ controllers/users.js
@@ -12,6 +12,18 @@ usersRouter.post('/', async (req, res) => {
     })
   }
 
+  if (username.length < 5) {
+    return res.status(400).json({
+      error: 'username must contain at least 5 characters'
+    })
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({
+      error: 'password must contain at least 6 characters'
+    })
+  }
+
   const passwordHash = await bcrypt.hash(password, 10)
 
   const user = new User({
@@ -23,6 +35,11 @@ usersRouter.post('/', async (req, res) => {
   const savedUser = await user.save()
 
   res.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
 })
 
 module.exports = usersRouter
