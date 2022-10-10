980a0b33a7afe87f1e613385bf146be2bd12296d
Tobi Balogun <tobi.balo@gmail.com>
2022-10-11 00:16:40 +0100

    func: update error handling middleware with support for JsonWebToken errors

--- utils/middleware.js
+++ utils/middleware.js
@@ -13,12 +13,20 @@ const unknownEndpoint = (req, res, next) => {
 }
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error)
+  logger.error(error.message)
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({
+      error: 'invalid token'
+    })
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).json({
+      error: 'token expired'
+    })
   }
 
   next(error)
